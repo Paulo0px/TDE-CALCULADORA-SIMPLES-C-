116b0222df8b31066b2652606953a41f50449f34
Paulo0px <paulo_hqs@outlook.com>
2024-08-22 20:56:26 -0300

    TDE CALCULADORA SIMPLES USANDO C#

---
+++ tde/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Digite o primeiro número: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int soma = num1 + num2;
+int subtracao = num1 - num2;
+int divisao = num1 / num2;
+int multiplicacao = num1 * num2;
+
+//usando concatenação
+Console.WriteLine("A soma é: " + soma);
+
+//usando interpolação
+Console.WriteLine($"A subtração é: {subtracao}");
+
+//usando placeholders
+Console.WriteLine("A divisão é: {0}", divisao);
+Console.WriteLine("A multiplicação é: {0}", multiplicacao);
+
+
